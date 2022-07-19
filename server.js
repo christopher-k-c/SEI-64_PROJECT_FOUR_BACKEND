978ea09a8f2367ec9c03844d34888be27c5145d6
christopher-k-c <chriscareyk@gmail.com>
2022-07-19 10:40:23 +0100

    Product Model

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
+app.use(express.json())
 
 const PORT = process.env.PORT;
 
 const indexRouter = require("./routes/index")
+const productCtrl = require("./routes/product")
 
 app.use("/", indexRouter)
+app.use("/", productCtrl)
 
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
 
