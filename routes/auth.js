d1c37e535a225f5f58fb3d1e906bd74f254c2509
cailey90 <chris.ailey90@gmail.com>
2022-07-21 22:24:46 +0100

    implemented auth_user_get API; made all properties of Product schema required except URL

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,12 @@
-const router = require("express").Router();
+const express = require("express")
+
+const router = express.Router();
 
 //const { attachment } = require("express/lib/response");
 
 const authCtrl = require("../controllers/auth");
 
+router.get("/auth/users", authCtrl.auth_user_get);
 //router.get("/auth/signup", authCtrl.auth_signup_get);
 router.post("/auth/signup", authCtrl.auth_signup_post);
 //router.get("/auth/login", authCtrl.auth_login_get);
