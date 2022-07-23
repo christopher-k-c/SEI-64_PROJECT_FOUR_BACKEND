f03a37733bd4b45e801b00cebba73b38cb95198d
cailey90 <chris.ailey90@gmail.com>
2022-07-23 23:22:23 +0100

    implemented /product/update PUT api

--- routes/product.js
+++ routes/product.js
@@ -2,15 +2,16 @@ const express= require("express");
 
 const router = express.Router()
 
+router.use(express.json())
 
 const productCtrl = require("../controllers/product")
 
 router.post("/product/add", productCtrl.product_index_post)
 router.get("/product/index", productCtrl.product_index_get)
-router.put("/product/update", productCtrl.product_update_put)
 router.delete("/product/delete", productCtrl.product_delete_get);
 router.get("/product/detail", productCtrl.product_detail_get);
 router.get("/product/edit", productCtrl.product_edit_get)
+router.put("/product/update", productCtrl.product_update_put)
 
 
 module.exports = router;
